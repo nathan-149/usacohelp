dd22789a2cf96493e082a62985eeed592410197d
michael153 <michaelruihaowan@gmail.com>
2015-05-25 16:50:08 -0700

    USACOHelp

--- upload.php
+++ upload.php
@@ -1,5 +1,5 @@
 <?php
-move_uploaded_file ($_FILES['uploadFile'] ['tmp_name'], "../uploads/{$_FILES['uploadFile']['name']}")
+move_uploaded_file($_FILES['file'] ['tmp_name'], "/uploads/{$_FILES['file']['name']}")
 if($_POST[upload] == '1')
 {
 	$to = "uploads/".$_FILES['file']['name'];
