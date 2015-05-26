16b5b81b70ae5d25ea1597496886955b960c8436
michael153 <michaelruihaowan@gmail.com>
2015-05-25 23:06:41 -0700

    Website

--- upload.php
+++ upload.php
@@ -1,9 +1,7 @@
 <?php
-move_uploaded_file($_FILES['file'] ['tmp_name'], "/uploads/{$_FILES['file']['name']}")
 if($_POST[upload] == '1')
 {
-	$to = "uploads/".$_FILES['file']['name'];
-	move_uploaded_file($_FILES)['file']['tmp_name'], $to);
+	move_uploaded_file($_FILES['file']['tmp_name'], "/uploads/{$_FILES['file']['name']}")
 	echo "Uploaded";
 }
 ?>
